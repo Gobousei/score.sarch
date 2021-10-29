7207f6d4af5fa5f1c8849764e0a1d31930a45c49
Gobousei <93245017+Gobousei@users.noreply.github.com>
2021-10-29 12:26:30 +0900

    write messages

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
-function butotnClick(){
-  const t1 = document.getElementById("text1").value;
-    document.getElementById("span1").textContent = t1;
-       var spreadsheet = SpreadsheetApp.openById('1-6TzqeB0J5RxylGa4GD-lBXfvRJkU8UhP1Tt6QuXURE')
-        var sheet = spreadsheet.getActiveSheet();
-        
-        var textFinder = sheet.createTextFinder(t1);
-        var cells = textFinder.findAll();
-        
-        for(var i=0; i<cells.length; i++){
-          var ss = cells[i].getA1Notation();
-const range = sheet.getRange(ss);
-  console.log(range.getValue());
-}
+    const showMessage = () => {
+      const textbox = document.getElementById("input-message");
+      const inputValue = textbox.value;
 
-
-let msg = document.getElementById('msg');
-
-let checkButton = document.getElementById('checkButton');
-checkButton.addEventListener('click', butotnClick);
+      //テキストボックスの値を使って、出力するメッセージを生成する
+      const output = "入力された内容は「" + inputValue + "」です。";
+      //出力用のp要素にメッセージを表示
+      document.getElementById("output-message").innerHTML = output;
+    }

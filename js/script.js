44963df80356cab4a89af4127a10440f35269a8d
Gobousei <93245017+Gobousei@users.noreply.github.com>
2021-10-29 14:33:06 +0900

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+    const showMessage = () => {
+      const textbox = document.getElementById("input-message");
+      const inputValue = textbox.value;
+      document.getElementById("input-message").innerHTML = output;
+    }

305a43437df02d214e5a5eeba7b7a46aed51ea63
Gobousei <93245017+Gobousei@users.noreply.github.com>
2021-10-29 15:55:18 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-    const showMessage = () => {
+<script lang="ja">
       const textbox = document.getElementById("input-message");
       const inputValue = textbox.value;
       document.getElementById("input-message").innerHTML = output;
-    }
+</script>
